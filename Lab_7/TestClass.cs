6db77da78437dba0dafd473c52fd98099c91e224
Artur Safiullin <shard.f@mail.ru>
2020-04-11 12:40:51 +0300

    Add changes

---
+++ Lab_7/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7
+{
+    public class TestClass
+    {
+        public TestClass()
+        {
+
+        }
+
+        public static void CheckArthur()
+        {
+            Console.WriteLine("Arthur");
+        }
+    }
+}
